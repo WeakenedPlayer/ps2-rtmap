e346c1d1224cbd933fe5575bfa70d140d6034005
WeakenedPlayer <weakenedplayer@gmail.com>
2017-02-04 03:14:40 +0900

    検索機能追加(制限が必要)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CensusService } from './service/census/census.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor() {
+    }
+    
+    ngOnInit() {
+    }
 }
--- src/app/view/character-search/character-search.component.ts
+++ src/app/view/character-search/character-search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CensusService } from '../../service/census/census.service';
 
 @Component({
   selector: 'character-search',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-search.component.scss']
 })
 export class CharacterSearchComponent implements OnInit {
+    @Input() characterName: string;
+    candidates: string[] = [];
+    census: CensusService;
+    answer: any[] = [];
+    constructor( census: CensusService ) {
+        this.census = census;
+    }
 
-  constructor() { }
-
-  ngOnInit() {
+    ngOnInit() {
   }
-
+    search( name: string ) {
+        console.log( name );
+        if( name.length > 4 ){
+            this.census.findCharacterName( name.toLowerCase(), ( result ) => { this.answer = result; console.log( result ); }  );
+        }
+    }
 }
